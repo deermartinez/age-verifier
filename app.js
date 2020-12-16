b3ad771efe78a94338c13fd2329a3ca25f536227
Destynee Martine <deermartinez8@gmail.com>
2020-12-15 19:36:59 -0700

    else if and loops

--- app.js
+++ app.js
@@ -1,10 +1,67 @@
+// document.write('Hello World' + '<br/>');
+// document.write('Hello World' + '<br/>');
+// document.write('Hello World' + '<br/>');
+// document.write('Hello World' + '<br/>');
+// document.write('Hello World' + '<br/>');
+// document.write('Hello World' + '<br/>');
+// document.write('Hello World' + '<br/>');
+// document.write('Hello World' + '<br/>');
+// document.write('Hello World' + '<br/>');
+// document.write('Hello World' + '<br/>');
 
 
+// for (i-0; 1<10; i++){
+// document.write("Hello WOrlds" + '<br/>');
+// }
 
-for(i=0; i<10; i++){
-    if (i===7){
-        break;
+
+// for(i=0; i<10; i++){
+//     if (i===7){
+//         break;
+//     }
+// }
+
+// document.write ("The number is" + i + "<br>");
+
+// var x = 0;
+// while(x < 5){
+//     console.log(x);
+//     x = x + 1;
+// };
+
+// varx  = 5;
+// while(x<5){
+//     console.log(x);
+//     x = x + 1;
+// }
+//FIRST ATTEMPT
+// for ( var i=0; i <= 20; i++){
+//     if (i===("1", "3", "5", "7")){
+//         document.write(i + " is odd");
+//     }
+//     if (i==="2", "4", "6" ){
+//         document.write(i + " is even");
+//     }
+// }
+
+for ( var i=0; i <= 20; i++){
+    if(i % 2 === 0){
+            document.write(i + "is even" + "<br>");
+    }
+    else {
+        document.write(i + "is odd" + "<br>");
     }
 }
 
-document.write ("The number is" + i + "<br>");
+var money = 2500.00;
+var bills = 2500.00;
+
+if (money > bills){
+     document.write ("You're good to go!");
+}
+else if (money===bills){
+    document.write("Don't push it");
+}
+    else {
+        document.write("Haha POOR");
+};
